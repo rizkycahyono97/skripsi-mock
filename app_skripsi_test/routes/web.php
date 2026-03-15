25aba49657b47dba5a75b129b1a3954666da982a
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2026-03-15 22:15:41 +0700

    add routing

--- app_skripsi_test/routes/web.php
+++ app_skripsi_test/routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Http\Controllers\DocumentController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+    
+    // document
+    Route::get('documents', [DocumentController::class, 'index'])->name('documents.index');
+    Route::post('documents', [DocumentController::class, 'post'])->name('documents.store');
 });
 
 require __DIR__.'/settings.php';
