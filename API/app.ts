dd05cde9a313e32a92268c540d0c74430fc7c6d6
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2026-05-31 21:57:07 +0700

    add logger in API

--- API/app.ts
+++ API/app.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import express, { Express, ErrorRequestHandler } from 'express';
 import { corsMiddleware, errorHandler } from './src/middleware/corsMiddleware';
 import documentRoutes from './src/route/documentRoutes';
+import { traceMiddleware } from './src/middleware/traceMiddleware';
+import { logger } from './src/utils/logger';
 
 const app: Express = express();
 
@@ -9,11 +11,13 @@ app.use(corsMiddleware);
 
 app.use(express.json());
 
+app.use(traceMiddleware);
+
 app.use('/api/v1', documentRoutes);
 
 app.use(errorHandler);
 
 const PORT: number = parseInt(process.env.PORT || '3000', 10);
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
--- API/src/services/blockchainService.ts
+++ API/src/services/blockchainService.ts
@@ -2,6 +2,7 @@ import { ethers, Wallet } from 'ethers';
 import { provider, getContract } from '../config/blockchain';
 import { domain, types } from '../constants/eip712';
 import { DocumentPayload } from '../types/services/document';
+import { logger } from '../utils/logger';
 
 export const signAndIssueDocument = async (
   payload: DocumentPayload
@@ -12,14 +13,29 @@ export const signAndIssueDocument = async (
     throw new Error('Validator private key not found in environment variables');
   }
 
+  logger.info(
+    `[Blockchain Service][signAndIssueDocument] Begining: ${JSON.stringify(payload)}`
+  );
+
   const wallet: Wallet = new ethers.Wallet(validator, provider);
   const contract = getContract(wallet);
 
   const signature: string = await wallet.signTypedData(domain, types, payload);
+  logger.info(
+    `[Blockchain Service][signAndIssueDocument] Signing: ${signature}`
+  );
 
   const feeData = await provider.getFeeData();
   const tx = await contract.issueDocument(payload, signature);
+
+  logger.info(
+    `[Blockchain Service][signAndIssueDocument] Transaction Succesfully. Tx Hash: ${tx.hash}`
+  );
+
   const receipt = await tx.wait();
+  logger.info(
+    `[Blockchain Service][signAndIssueDocument]] Transaction Succesfully, Block #${receipt.blockNumber}! Gas Used: ${receipt.gasUsed.toString()}`
+  );
 
   return receipt;
 };
