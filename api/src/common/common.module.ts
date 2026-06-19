196aa49d32258fb5c2f9eab8b24d5dc6e428ba01
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2026-06-19 20:38:51 +0700

    add winston and validation service

---
+++ api/src/common/common.module.ts
@@ -0,0 +1,53 @@
+import { Global, Module } from '@nestjs/common';
+import { ValidationService } from './validation.service';
+import {
+  WinstonModule,
+  utilities as nestWinstonModuleUtilities,
+} from 'nest-winston';
+import * as winston from 'winston';
+import { ConfigModule } from '@nestjs/config';
+
+@Global()
+@Module({
+  imports: [
+    WinstonModule.forRoot({
+      format: winston.format.json(),
+      transports: [
+        // log terminal
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike('API', {
+              colors: true,
+              appName: true,
+            }),
+          ),
+        }),
+        //log app
+        new winston.transports.File({
+          filename: 'logs/error.log',
+          level: 'error',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.json(),
+          ),
+        }),
+        // log blockchain
+        new winston.transports.File({
+          filename: 'logs/blockchain.log',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.json(),
+          ),
+        }),
+      ],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  providers: [ValidationService],
+  exports: [ValidationService],
+})
+export class CommonModule {}
